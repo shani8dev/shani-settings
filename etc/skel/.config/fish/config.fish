463080eab951bbb289156b8e6c34c5de1217350e
Shrinivas Kumbhar <shrinivas.v.kumbhar@gmail.com>
2024-10-31 23:05:20 +0530

    initial config

--- etc/skel/.config/fish/config.fish
+++ etc/skel/.config/fish/config.fish
@@ -104,9 +104,6 @@ alias .... 'cd ../../..'
 alias ..... 'cd ../../../..'
 alias ...... 'cd ../../../../..'
 alias dir 'dir --color=auto'
-alias grep 'ugrep --color=auto'
-alias egrep 'ugrep -E --color=auto'
-alias fgrep 'ugrep -F --color=auto'
 alias hw 'hwinfo --short'                          # Hardware Info
 alias ip 'ip -color'
 alias psmem 'ps auxf | sort -nr -k 4'
@@ -116,12 +113,14 @@ alias untar 'tar -zxvf '
 alias vdir 'vdir --color=auto'
 alias wget 'wget -c '
 
-# Get fastest mirrors
-alias mirror 'sudo reflector -f 30 -l 30 --number 10 --verbose --save /etc/pacman.d/mirrorlist'
-alias mirrora 'sudo reflector --latest 50 --number 20 --sort age --save /etc/pacman.d/mirrorlist'
-alias mirrord 'sudo reflector --latest 50 --number 20 --sort delay --save /etc/pacman.d/mirrorlist'
-alias mirrors 'sudo reflector --latest 50 --number 20 --sort score --save /etc/pacman.d/mirrorlist'
-
 # Get the error messages from journalctl
 alias jctl 'journalctl -p 3 -xb'
 
+# Load Mcfly
+set -Ux MCFLY_FUZZY true
+set -Ux MCFLY_RESULTS 20
+set -Ux MCFLY_INTERFACE_VIEW BOTTOM
+set -Ux MCFLY_RESULTS_SORT LAST_RUN
+
+# Initialize Mcfly
+mcfly init fish | source
