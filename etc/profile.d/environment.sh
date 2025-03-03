7bef3e3c03d90b3d28dfb761851f1e077e5900e0
Shrinivas Kumbhar <shrinivas.v.kumbhar@gmail.com>
2025-03-03 14:28:31 +0530

    update settings mime types and add psd config

--- etc/profile.d/environment.sh
+++ etc/profile.d/environment.sh
@@ -1,12 +1,10 @@
-#!/bin/bash
-
 # If it's a Wayland session, set some environment variables.
-
 if [ "$XDG_SESSION_TYPE" = "wayland" ]; then
-	export QT_QPA_PLATFORM=wayland
-	export MOZ_ENABLE_WAYLAND=1
+    export QT_QPA_PLATFORM=wayland
+    export GDK_BACKEND=wayland
+    export MOZ_ENABLE_WAYLAND=1
 fi
 
-# Set some other environment variables
+export QT_AUTO_SCREEN_SCALE_FACTOR=1
+export QT_STYLE_OVERRIDE=gtk
 
-export QT_STYLE_OVERRIDE=kvantum
